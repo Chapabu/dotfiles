2ce15597fbf0c760731b285eecac673d8bf7e642
Matt Chapman <chapabu101@gmail.com>
2018-08-02 11:56:49 +0100

    Added some feedback.

--- .functions.sh
+++ .functions.sh
@@ -6,5 +6,7 @@
 # This is not run on terminal load because it takes forever.
 ###
 function load_drupal_console() {
+  echo "Loading Drupal Console config..."
   source "$HOME/.console/console.rc" 2>/dev/null
+  echo "Done 😎"
 }
