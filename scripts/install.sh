a91e795df25e6fd0513fde9146cadfb2aeaba384
Matt Chapman <mattc@kevinc.office.cogapp.com>
2020-03-11 11:28:45 +0000

    Install Spotify

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,6 @@ source ${PWD}/install/apt.sh
 source ${PWD}/install/docker.sh
 source ${PWD}/install/tmuxinator.sh
 source ${PWD}/install/python.sh
-source ${PWD}/install/snap.sh
+source ${PWD}/install/spotify.sh
 source ${PWD}/install/vim.sh
 source ${PWD}/install/zsh.sh
